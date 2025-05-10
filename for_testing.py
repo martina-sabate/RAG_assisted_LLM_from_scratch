0d4c3af2e89a2e320d98364e4b14b6b698c751ad
martina-sabate <martina.monfort.20@ucl.ac.uk>
2025-05-10 22:48:50 +0100

    2.4

--- for_testing.py
+++ for_testing.py
@@ -1,21 +1,12 @@
-import os
-import requests
-import fitz
+import spacy
+import medspacy
 
-print(fitz.__file__)
-#from tqdm.auto import tqdm
-#Note: tqdm is used to obtain a progress bar from any loop you run with it
-
-# Get PDF path 
-# Change this variable to your pdf path______________________________
-pdf_path = r"G:\My Drive\feines 2025\MS Imaging paper\to submit.pdf"
-#____________________________________________________________________
-
-# Check that the path exists
-if os.path.exists(pdf_path):
-    print(f"PDF file '{pdf_path}' exists.")
-else:
-    print(f"PDF file '{pdf_path}' does not exist")
-
-# Open the PDF file
-document = fitz.open(pdf_path)
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("John Smith presented with speech delay on 12/03/2022.")
+print("test")
+print(doc.ents)
+for ent in doc.ents:
+    if ent._.is_phi:  # spaCy extension
+        print(ent.text, ent.label_)
+    else:
+        print("no")   
